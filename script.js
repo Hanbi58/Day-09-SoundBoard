5be84a84fc2311e4a6194e23a31fdb0ef4f88521
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-31 12:58:11 -0700

    my version, but teacher's idea!

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('1')
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const buttons = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.innerText = sound;
+  buttons.appendChild(btn);
+  btn.addEventListener("click", () => {
+    sounds.forEach((sound) => {
+      const ado = document.getElementById(sound);
+      ado.pause();
+    });
+    document.getElementById(sound).play();
+  });
+});
